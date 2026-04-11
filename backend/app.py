f294a06eb0139e9a8dd484b0fdb7f698de6436cb
psrivarshika00 <psrivarshika00@gmail.com>
2026-04-10 20:02:50 -0700

    Add EB deployment config (wsgi.py, Procfile, gunicorn)

--- backend/app.py
+++ backend/app.py
@@ -523,5 +523,6 @@ def get_summaries():
 
 # ------------------ Main ------------------
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5001)
+    port = int(os.environ.get("PORT", 5001))
+    app.run(debug=True, host="0.0.0.0", port=port)
 
