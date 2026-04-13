9f5c896b1483f9e971505932a70b5de61458bb9c
psrivarshika00 <psrivarshika00@gmail.com>
2026-04-13 15:38:04 -0700

    Add backend API URL for EC2 deployment

--- backend/app.py
+++ backend/app.py
@@ -698,5 +698,5 @@ def get_summaries():
 # ------------------ Main ------------------
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5001))
-    app.run(debug=True, host="0.0.0.0", port=port)
+    app.run(debug=False, host="0.0.0.0", port=port)
 
