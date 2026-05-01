d278a68c11fd6066f81dd14b4829ead5ace6a70b
psrivarshika00 <psrivarshika00@gmail.com>
2026-05-01 09:15:27 -0700

    Update backend IP for new deployment

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -7,7 +7,7 @@ module.exports = {
       cwd: '/home/ubuntu/Legal-Document-Analyzer---final-project-/frontend',
       env: {
         NODE_ENV: 'production',
-        NEXT_PUBLIC_API_URL: 'http://35.174.18.100:5001',
+        NEXT_PUBLIC_API_URL: 'http://18.207.232.182:5001',
       },
     },
   ],
